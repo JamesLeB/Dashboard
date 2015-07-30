c3da5437d801b7b5df0f93da3508b363d63901a5
JamesLeB <james@jamesleblanc.org>
2015-07-30 11:47:57 -0400

    Add primary buttons to dashboard

--- index.php
+++ index.php
@@ -12,5 +12,19 @@ if(isset($_SESSION['user'])){
 </head>
 <body>
 	<p>Welcome to the Dashboard</p>
+	<table>
+		<tr>
+			<td>Create EDI 270 request</td>
+			<td><button>ETL-1</button></td>
+		</tr>
+		<tr>
+			<td>Create final report</td>
+			<td><button>ETL-2</button></td>
+		</tr>
+		<tr>
+			<td>Clear the line</td>
+			<td><button>ETL-3</button></td>
+		</tr>
+	</table>
 </body>
 </html>
